3902b6fef08713f2bf8dca85612c4f3578fdf303
MariiaSheremet <grishkomariya@gmail.com>
2024-01-08 22:00:14 +0100

    changed

--- src/script.js
+++ src/script.js
@@ -1,21 +1,20 @@
-function displayWeather(response) {
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
-  temperatureElement.innerHTML = temperature;
-
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = temperature;
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-
   let city = searchInputElement.value;
-  let apiKey = "dc44b0aotf0b3e03700a44f31f7ca829";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayWeather);
+  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
 }
 
 function formatDate(date) {
